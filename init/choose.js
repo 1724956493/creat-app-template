21f622766af10cc962d4bc5565a81d30c3d5d1ab
1724956493@qq.com <1724956493@qq.com>
2021-03-17 10:32:47 +0800

    init

---
+++ init/choose.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+
+module.exports = async function () {
+    const { source } = await inquirer.prompt([{
+        type: 'list',
+        name: 'source',
+        message: '请选择模版',
+        choices: ["Vue2"]
+    }]);
+    return new Promise((resolve, reject) => {
+        if (source === 'Vue2') {
+            resolve("direct:https://gitee.com/ryfn-lip/template-vue2.git")
+        } else {
+            reject('选择模版错误')
+        }
+    })
+}
