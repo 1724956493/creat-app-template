4f610f326c77bad3356f5ab7d1f87bcfce9bd33a
lip_mac <1724956493@qq.com>
2022-08-10 09:25:29 +0800

    添加风云-智慧城市前端开发模板

--- init/choose.js
+++ init/choose.js
@@ -1,19 +1,29 @@
-const inquirer = require('inquirer')
+const inquirer = require("inquirer");
 
 module.exports = async function () {
-    const { source } = await inquirer.prompt([{
-        type: 'list',
-        name: 'source',
-        message: '请选择模版',
-        choices: ["Vue2", "uniapp"]
-    }]);
-    return new Promise((resolve, reject) => {
-        if (source === 'Vue2') {
-            resolve("direct:https://gitee.com/ryfn-lip/app-template-vue2.git")
-        } else if (source === 'uniapp') {
-            resolve("direct:https://gitee.com/ryfn-lip/app-template-uniapp.git")
-        } else {
-            reject('选择模版错误')
-        }
-    })
-}
+  const { source } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "source",
+      message: "请选择模版",
+      choices: ["Vue2", "风云-智慧城市前端开发模板", "uniapp"],
+    },
+  ]);
+  return new Promise((resolve, reject) => {
+    switch (source) {
+      case "Vue2":
+        resolve("direct:https://gitee.com/ryfn-lip/app-template-vue2.git");
+        break;
+      case "风云-智慧城市前端开发模板":
+        resolve(
+          "direct:http://fy-part2-gitlab.cnsaas.com/project-library/XM-YX-202203-5013/economic-web-demo.git"
+        );
+        break;
+      case "uniapp":
+        resolve("direct:https://gitee.com/ryfn-lip/app-template-uniapp.git");
+        break;
+      default:
+        reject("选择模版错误");
+    }
+  });
+};
