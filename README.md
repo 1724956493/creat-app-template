0664056de2afa9daebcaa70c63e52170a5b0c148
lip_mac <1724956493@qq.com>
2023-05-04 15:45:25 +0800

    1

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 npx  moon-template init my-app
 ```
 > Vue2
+> Vue3
+> 风云-智慧城市前端开发模板
+> uniapp-vue2
+> uniapp-vue3
--- init/choose.js
+++ init/choose.js
@@ -6,7 +6,7 @@ module.exports = async function () {
       type: "list",
       name: "source",
       message: "请选择模版",
-      choices: ["Vue2", "风云-智慧城市前端开发模板", "uniapp"],
+      choices: ["Vue2", "Vue3", "风云-智慧城市前端开发模板", "uniapp-vue2", "uniapp-vue3"],
     },
   ]);
   return new Promise((resolve, reject) => {
@@ -14,13 +14,19 @@ module.exports = async function () {
       case "Vue2":
         resolve("direct:https://gitee.com/ryfn-lip/app-template-vue2.git");
         break;
+      case "Vue3":
+        resolve("direct:https://gitee.com/ryfn-lip/app-template-vue3.git");
+        break;
       case "风云-智慧城市前端开发模板":
         resolve(
           "direct:http://fy-part2-gitlab.cnsaas.com/project-library/XM-YX-202203-5013/economic-web-demo.git"
         );
         break;
-      case "uniapp":
+      case "uniapp-vue2":
         resolve("direct:https://gitee.com/ryfn-lip/app-template-uniapp.git");
+        break;
+      case "uniapp-vue3":
+        resolve("direct:https://gitee.com/ryfn-lip/app-template-uniapp.git#vue3");
         break;
       default:
         reject("选择模版错误");
